db98c2f150ccf3f2a0b56255b5e585eedd096b62
Chaz Selph <chaz@askkanzi.com>
2022-01-10 19:16:45 -0500

    Fix enqueue function for order of loading bootstrap, style.css, fontawesome

--- wp-content/themes/askkanzi/front-page.php
+++ wp-content/themes/askkanzi/front-page.php
@@ -10,11 +10,6 @@
     <meta name="author" content="https://chazselph.github.io">    
     <link rel="shortcut icon" href="images/logo.png"> 
     
-    <!-- FontAwesome CSS-->
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
-	<!-- Bootstrap CSS-->
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">  
-	<!-- Theme CSS -->
 
 	<?php
 
