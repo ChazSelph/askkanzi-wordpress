c312fd3d06fdc64ab3d8265fd02309df1ff58b65
Chaz Selph <chaz@askkanzi.com>
2022-01-14 22:15:15 -0500

    Add more images, redo posts

--- wp-content/themes/askkanzi/archive.php
+++ wp-content/themes/askkanzi/archive.php
@@ -3,25 +3,16 @@
 ?>
     
 		<article class="content px-3 py-5 p-md-5">
-
 				<?php 
-
 					if(have_posts()){
 						while(have_posts()){
-							the_post();
-							the_title();
+							// the_post();
 							get_template_part('template-parts/content', 'archive');
 						}
 					}
-
-
 				?>
-
-		
 	    </article>
 	    
-
-
     <?php 
 	get_footer();
 	?>
--- wp-content/themes/askkanzi/front-page.php
+++ wp-content/themes/askkanzi/front-page.php
@@ -10,13 +10,13 @@
 			<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
 				<div class="carousel-inner">
 				  <div class="carousel-item active">
-					<img src="images/slider2.jpg" class="d-block w-100" alt="1">
+					<img src="/wordpress/wp-content/themes/askkanzi/assets/images/kanzi-lights-fire.png" class="d-block w-100" alt="1">
 				  </div>
 				  <div class="carousel-item">
-					<img src="images/slider1.jpg" class="d-block w-100" alt="2">
+					<img src="/wordpress/wp-content/themes/askkanzi/assets/images/teco-touches-ipad.png" class="d-block w-100" alt="2">
 				  </div>
 				  <div class="carousel-item">
-					<img src="images/slider3.jpg" class="d-block w-100" alt="3">
+					<img src="/wordpress/wp-content/themes/askkanzi/assets/images/matata-looks-distance.png" class="d-block w-100" alt="3">
 				  </div>
 				</div>
 				<a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
@@ -29,20 +29,15 @@
 				</a>
 			  </div>
 			  <section class="theme-bg-dark py-5 mt-4 text-center">
-				<h3 class='text-light d-block'>Subscribe to the Newsletter</h3>
-			    <form class="signup-form form-inline justify-content-center pt-3">
-                    <div class="form-group">
-                        <label class="sr-only" for="semail">Your email</label>
-                        <input type="email" id="semail" name="semail1" class="form-control mr-md-1 semail" placeholder="Enter email">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Subscribe</button>
-                </form>
+				<h3 class='text-light d-block'>Read about the Legal Petition </h3>
+				<h3 class='text-light d-block'>against Ape Initiative</h3>
+                    <button type="submit" class="btn btn-primary">Read here</button>
 				</section>
 			<div class="row row-cols-1 mt-4 row-cols-md-2">
 				<div class="col mb-4">
 				  <div class="card">
 					<div class="card-body">
-					  <h5 class="card-title">Card title</h5>
+					  <h5 class="card-title">Interspecies Internet</h5>
 					  <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
 					</div>
 				  </div>
@@ -59,12 +54,11 @@
 	    </article>
 
 
-		<article class="content px-3 py-5 p-md-5">
+		<article class="front-page-p" >
 			<?php 
 				if(have_posts()){
 					while(have_posts()){
 						the_post();
-						the_title();
 						the_content();
 					}
 				}
